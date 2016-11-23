48f7e343133f07efa371bbc1a268b7fe6137c8fc
Luke <luke@ustwo.com>
2016-11-23 17:26:10 +0000

    Initial commit

---
+++ app/src/main/java/com/lost/calculator/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.lost.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+}
