e495f72fe1eecb2dce4865acada60e3c21f6ea10
Luke <luke@ustwo.com>
2016-11-23 18:08:08 +0000

    Add initial calculator layout

--- app/src/main/java/com/lost/calculator/CalculatorActivity.java
+++ app/src/main/java/com/lost/calculator/CalculatorActivity.java
@@ -1,7 +1,10 @@
 package com.lost.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator);
+    }
+
+    public void buttonOnClick(View view) {
+        Log.d("CalculatorActivity", ((TextView) view).getText().toString());
     }
 }
